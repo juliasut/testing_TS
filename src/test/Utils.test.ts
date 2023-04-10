f3b9146b446032959b80377646e30f57b7a86c4e
juliasut <juliasutygina@gmail.com>
2023-04-09 23:24:30 -0600

    Add tests with mathcers for getStringInfo method

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,4 +1,4 @@
-import { toUpperCase } from '../app/Utils';
+import { getStringInfo, toUpperCase } from '../app/Utils';
 
 describe('Utils test suite', () => {
   test('should return uppercase', () => {
@@ -11,5 +11,21 @@ describe('Utils test suite', () => {
 
     // assert:
     expect(actual).toBe(expected);
+  });
+
+  it('should return info for a valid string', () => {
+    const actual = getStringInfo('My-String');
+
+    expect(actual.lowerCase).toBe('my-string');
+    expect(actual.extraInfo).toEqual({});
+    expect(actual.characters).toHaveLength(9);
+    expect(actual.characters).toContain<string>('M');
+    expect(actual.characters).toEqual(
+      expect.arrayContaining(['S', 't', 'r', 'i', 'n', 'g', 'M', 'y'])
+    );
+    expect(actual.extraInfo).not.toBe(undefined);
+    expect(actual.extraInfo).not.toBeUndefined();
+    expect(actual.extraInfo).toBeDefined();
+    expect(actual.extraInfo).toBeTruthy();
   });
 });
